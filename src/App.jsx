b4ae36ebb5b9757fd21ac45ea2d18fd0f9884300
CraigB73 <craigbritton73@gmail.com>
2024-09-24 15:37:18 +0200

    Test to fix problem with Heroku

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,21 @@
-import styles from './App.module.css';
-import NavBar from './components/NavBar'
-import Container from 'react-bootstrap/Container'
-import { Route, Routes } from 'react-router-dom'
-import SignUpForm from './SignUpForm';
-
+// import styles from './App.module.css';
+// import NavBar from './components/NavBar'
+// import Container from 'react-bootstrap/Container'
+// import { Route, Routes } from 'react-router-dom'
+// import SignUpForm from './SignUpForm';
+// className={styles.App}
 function App() {
   return (
-    <div className={styles.App}>
-      <NavBar />
+    <div >
+      <h1>Hello There!</h1>
+      {/* <NavBar />
       <Container className={styles.Main}>
         <Routes>
           <Route path='/' element={<h1>Home page</h1>} />
           <Route path='/signin'element ={<h1>Sign in</h1>} />
           <Route path='/signup'element ={<SignUpForm/>} />
         </Routes>
-      </Container>
+      </Container> */}
       
     </div>
   )
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,54 +1,54 @@
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import { NavLink } from "react-router-dom";
+// import Container from 'react-bootstrap/Container';
+// import Nav from 'react-bootstrap/Nav';
+// import Navbar from 'react-bootstrap/Navbar';
+// import { NavLink } from "react-router-dom";
 
-import logo from '../assets/images/sharedmoments-logo.png'
-import styles from '../styles/NavBar.module.css'
+// import logo from '../assets/images/sharedmoments-logo.png'
+// import styles from '../styles/NavBar.module.css'
 
 
-const NavBar = () => {
-    return (
-        <Navbar expand="md" fixed="top" className={styles.NavBar} >
-            <Container>
-							<NavLink to='/' end>
-									<Navbar.Brand>
-											<img
-													src={logo}
-													width="45"
-													height="45"
-													className="d-inline-block align-top"
-													alt="logo"
-											/>
-									</Navbar.Brand>
-							</NavLink>
-							<Navbar.Toggle aria-controls="basic-navbar-nav" />
-								<Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-									<Nav className='mr-auto d-flex align-items-start'>
-          <NavLink
-            to='/'
-            className={(isActive) => `${styles.NavLink} ${isActive ? 'active' : ''}`}
-          >
-             <i className='fas fa-home'></i>Home
-          </NavLink>
-          <NavLink
-            to='/signin'
-            className={(isActive ) => `${styles.NavLink} ${isActive ? 'active' : ''}`}
-          >
-            <i className='fas fa-sign-in-alt'></i>Sign in
-          </NavLink>
-          <NavLink
-            to='/signup'
-            className={(isActive ) => `${styles.NavLink} ${isActive ? 'active' : ''}`}
-          >
-            <i className='fas fa-user-plus'></i>Sign up
-          </NavLink>
-        </Nav>
-							</Navbar.Collapse>
-            </Container>
-          </Navbar>
+// const NavBar = () => {
+//     return (
+//         <Navbar expand="md" fixed="top" className={styles.NavBar} >
+//             <Container>
+// 							<NavLink to='/' end>
+// 									<Navbar.Brand>
+// 											<img
+// 													src={logo}
+// 													width="45"
+// 													height="45"
+// 													className="d-inline-block align-top"
+// 													alt="logo"
+// 											/>
+// 									</Navbar.Brand>
+// 							</NavLink>
+// 							<Navbar.Toggle aria-controls="basic-navbar-nav" />
+// 								<Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+// 									<Nav className='mr-auto d-flex align-items-start'>
+//           <NavLink
+//             to='/'
+//             className={(isActive) => `${styles.NavLink} ${isActive ? 'active' : ''}`}
+//           >
+//              <i className='fas fa-home'></i>Home
+//           </NavLink>
+//           <NavLink
+//             to='/signin'
+//             className={(isActive ) => `${styles.NavLink} ${isActive ? 'active' : ''}`}
+//           >
+//             <i className='fas fa-sign-in-alt'></i>Sign in
+//           </NavLink>
+//           <NavLink
+//             to='/signup'
+//             className={(isActive ) => `${styles.NavLink} ${isActive ? 'active' : ''}`}
+//           >
+//             <i className='fas fa-user-plus'></i>Sign up
+//           </NavLink>
+//         </Nav>
+// 							</Navbar.Collapse>
+//             </Container>
+//           </Navbar>
 
-  )
-}
+//   )
+// }
 
-export default NavBar
+// export default NavBar
