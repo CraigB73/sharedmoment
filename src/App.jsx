4f20014167592d60003fce08b2966ff0a7408f49
CraigB73 <craigbritton73@gmail.com>
2024-09-24 14:49:28 +0200

    Add nav styles and intitial routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
-import './App.css'
+import styles from './App.module.css';
 import NavBar from './components/NavBar'
+import Container from 'react-bootstrap/Container'
+import { Route, Routes } from 'react-router-dom'
+import SignUpForm from './SignUpForm';
+
 function App() {
   return (
-    <>
-    <NavBar/>
-    </>
+    <div className={styles.App}>
+      <NavBar />
+      <Container className={styles.Main}>
+        <Routes>
+          <Route path='/' element={<h1>Home page</h1>} />
+          <Route path='/signin'element ={<h1>Sign in</h1>} />
+          <Route path='/signup'element ={<SignUpForm/>} />
+        </Routes>
+      </Container>
+      
+    </div>
   )
 }
 
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,33 +1,53 @@
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from "react-router-dom";
 
 import logo from '../assets/images/sharedmoments-logo.png'
+import styles from '../styles/NavBar.module.css'
 
 
 const NavBar = () => {
-  return (
-    <Navbar expand="md" fixed="top" className="bg-body-secondary">
-        <Container className=''>
-            <Navbar.Brand>
-                <img
-                    src={logo}
-                    width="45"
-                    height="45"
-                    className="d-inline-block align-top"
-                    alt="logo"
-                />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
-            <Nav className='mr-auto d-flex align-items-start'>
-                <Nav.Link><i className='fas fa-home'></i>Home</Nav.Link>
-                <Nav.Link><i className='fas fa-sign-in-alt'></i>Sign in</Nav.Link>
-                <Nav.Link><i className='fas fa-user-plus'></i>Sign up</Nav.Link>
-            </Nav>
-            </Navbar.Collapse>
-        </Container>
-    </Navbar>
+    return (
+        <Navbar expand="md" fixed="top" className={styles.NavBar} >
+            <Container>
+							<NavLink to='/' end>
+									<Navbar.Brand>
+											<img
+													src={logo}
+													width="45"
+													height="45"
+													className="d-inline-block align-top"
+													alt="logo"
+											/>
+									</Navbar.Brand>
+							</NavLink>
+							<Navbar.Toggle aria-controls="basic-navbar-nav" />
+								<Navbar.Collapse className="justify-content-end" id="basic-navbar-nav">
+									<Nav className='mr-auto d-flex align-items-start'>
+          <NavLink
+            to='/'
+            className={(isActive) => `${styles.NavLink} ${isActive ? 'active' : ''}`}
+          >
+             <i className='fas fa-home'></i>Home
+          </NavLink>
+          <NavLink
+            to='/signin'
+            className={(isActive ) => `${styles.NavLink} ${isActive ? 'active' : ''}`}
+          >
+            <i className='fas fa-sign-in-alt'></i>Sign in
+          </NavLink>
+          <NavLink
+            to='/signup'
+            className={(isActive ) => `${styles.NavLink} ${isActive ? 'active' : ''}`}
+          >
+            <i className='fas fa-user-plus'></i>Sign up
+          </NavLink>
+        </Nav>
+							</Navbar.Collapse>
+            </Container>
+          </Navbar>
+
   )
 }
 
